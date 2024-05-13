1f446c5360632c014278395349fdb5181da40338
TheAwesomeTechGirl <tukeglory98@gmail.com>
2024-05-13 13:15:12 +0100

    fixed the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import Image from 'next/image';
+
 
 function Footer() {
   return (
-    <div className='footer bg-cyan-600 font-sans text-gray-100'>
-      <div className='p-10 md:p-5 '>
+    <div className='footer bg-cyan-600 text-gray-100'>
+      <div className=' w-full p-6 lg:max-w-[1200px] lg:mx-auto  md:p-5 '>
        <div className='justify-center items-center flex flex-col md:flex-row md:space-x-24'>
-       <div className='footerlogo flex flex-row  text-lg font-normal md:flex-col md:space-y-3'>
-          <img src='./footerLogo.svg' alt='footerlogo' className='w-12 h-12 md:w-36 md:h-36 mr-2' />
-          <p className='font-light text-sm md:text-base whitespace-normal '>
+       <div className='footerlogo  flex flex-row md:w-[30%] gap-6 mb-5 md:flex-col md:space-y-3'>
+          <img src='./footerLogo.svg' alt='footerlogo' className='w-10 h-10 md:w-24 md:h-24 mr-2' />
+          <p className='font-light text-[12px] md:text-[14px] '>
             Lorem ipsum dolor sit amet, 
-            <br/>
             consectetuer adipiscing elit.
-            <br/>
             Maecenas porttitor congue massa.
-            <br/>
              Fusce posuere, magna sed pulvinar ultricies,
           </p>
+          
         </div>
+      {/* <  flex flex-row text-sm font-light md:test-lg md:font-normal  md:flex-row mt-6 md:space-x-24> */}
 
-        <div className='Footeritems flex flex-row text-sm font-light md:test-lg md:font-normal  md:flex-row mt-6 md:space-x-24'>
-          <ul className='mb-4 md:mb-0 mr-4 space-y-3  md:space-y-8'>
+        <div className='Footeritems text-[12px] w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:text-[20px] md:text-[14px] font-normal md:w-[70%] leading-[28px] mt-3 mb-4'>
+          <ul className=' space-y-3 '>
             <li>Reach Us</li>
             <li>About Us</li>
             <li>Contact Us</li>
           </ul>
-
-          <ul className='mb-4 md:mb-0 mr-6 space-y-3  md:space-y-8'>
+          {/* mb-4 md:mb-0 mr-6 */}
+          <ul className=' space-y-3  '>
             <li>Programs</li>
             <li>Arts</li>
             <li>SDGs</li>
             <li>Entrepreneurship</li>
           </ul>
 
-          <ul className='space-y-3 md:space-y-8'>
+          <ul className='space-y-3 '>
             <li>Resources</li>
             <li>Blog</li>
             <li>Privacy Policy</li>
