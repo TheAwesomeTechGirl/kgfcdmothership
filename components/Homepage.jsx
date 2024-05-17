436ec7290faae1b919488afc8e7307692618105d
TheAwesomeTechGirl <tukeglory98@gmail.com>
2024-05-17 20:08:44 +0100

    styles the home page

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -4,16 +4,18 @@ function Homepage() {
   return (
     <div>
       <div
-        className="w-full h-screen bg-cover text-[#F1F1F1] "
+        className="w-full h-screen bg-cover bg-center flex items-center text-left text-[#F1F1F1]"
         style={{ backgroundImage: "url('/cuteblackbaby.svg')" }}
       >
-        <h1>OUR MANDATE</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
-          porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies,
-          purus lectus malesuada libero, sit amet commodo magna eros quis urna.
-          Nunc viverra imperdiet enim. Fusce est.
-        </p>
+        <div className="max-w-md mx-16">
+          <h1 className="text-4xl font-bold mb-4">OUR MANDATE</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
+            porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies,
+            purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+            Nunc viverra imperdiet enim. Fusce est.
+          </p>
+        </div>
       </div>
     </div>
   );
