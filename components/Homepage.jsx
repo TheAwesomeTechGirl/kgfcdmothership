cf3ddbe46037fb94cbf2f696f89d5991783bfd8b
TheAwesomeTechGirl <tukeglory98@gmail.com>
2024-05-18 04:19:08 +0100

    styles the home page

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import cuteblackbaby from "/public/cuteblackbaby.svg";
 
 function Homepage() {
   return (
     <div>
-      <div
-        className="w-full h-screen bg-cover bg-center flex items-center text-left text-[#F1F1F1]"
-        style={{ backgroundImage: "url('/cuteblackbaby.svg')" }}
-      >
-        <div className="max-w-md mx-16">
+      <div className="w-full h-screen bg-cover bg-center flex items-center text-left text-[#F1F1F1]">
+        <Image
+          src={cuteblackbaby}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          alt="Cute Black Baby"
+        />
+        <div className="max-w-md mx-16 z-10">
           <h1 className="text-4xl font-bold mb-4">OUR MANDATE</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
             porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies,
-            purus lectus malesuada libero, sit amet commodo magna eros quis urna.
-            Nunc viverra imperdiet enim. Fusce est.
+            purus lectus malesuada libero, sit amet commodo magna eros quis
+            urna. Nunc viverra imperdiet enim. Fusce est.
           </p>
         </div>
       </div>
--- components/Join.jsx
+++ components/Join.jsx
@@ -1,6 +1,8 @@
 "use client"; // Add this line at the top
 import React from "react";
+import Image from "next/image";
 import Button from "./Button";
+import assetImage from "../public/images/Asset.svg"; // Adjust the path to your Asset.svg
 
 function Join() {
   const handleClick = () => {
@@ -9,14 +11,14 @@ function Join() {
 
   return (
     <div
-      className="bg-[#E3B522] p-8  items-center justify-between text-[#1D1D1D] bg-opacity-100 relative bg-no-repeat bg-right"
-      style={{ backgroundImage: "url('./Asset.svg')", width: "100%" }}
+      className="bg-[#E3B522] p-8 items-center justify-between text-[#1D1D1D] bg-opacity-100 relative bg-no-repeat bg-right w-[20%]"
+      style={{ backgroundImage: `url(${assetImage.src})`, width: "100%" }}
     >
-      <div className="  space-y-6 ">
+      <div className="space-y-6">
         <h1 className="text-[15px] font-bold leading-[28px] sm:text-[25px] md:text-[16px] lg:text-[16px]">
           THE JOURNEY TO BUILDING A BETTER WORLD STARTS HERE WITH YOU
         </h1>
-        <p className="font-normal text-[16px]  md:text-[10px] lg:text-[12px] leading-[22.5px]">
+        <p className="font-normal text-[16px] md:text-[10px] lg:text-[12px] leading-[22.5px]">
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Maecenas
           porttitor congue massa. Fusce posuere,magna sed
           <br />
